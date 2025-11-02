67667cac59719c346da716653aa4ec290b3c0285
meenakshibathuluri <meenakshibathuluri@gmail.com>
2025-11-02 18:00:28 +0530

    javascript program uploaded

---
+++ Day-3/script.js
@@ -0,0 +1,55 @@
+//console.log(10+40);
+/*var a = 10;
+console.log(a);
+
+a = "meenakshi";
+console.log(a);
+
+console.log(typeof a);
+
+a = 12.8;
+console.log(a);
+
+console.log(typeof a);
+
+a = true;
+console.log(a);
+
+console.log(typeof a);
+*/
+/*let a = 10;
+console.log(a);
+
+a = "meenakshi";
+console.log(a);
+
+console.log(typeof a);
+
+a = 12.8;
+console.log(a);
+
+console.log(typeof a);
+
+a = true;
+console.log(a);
+
+console.log(typeof a);
+
+const a = 10;
+console.log(a);*/
+
+/*in let we should not print outside the if block.and let variableis block scoped
+if (true) {
+    let a = 10;
+    console.log("inside if block",a);
+}
+
+let a = 40;
+console.log("outside is block",a);*/
+
+/*var is global scope */ 
+if (true) {
+    var a = 30;
+    console.log("inside if block",a);
+}
+console.log("outside if block",a);
