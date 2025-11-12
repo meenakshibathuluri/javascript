8c49521ff9832bf2dc06d0b38867169a76d7eb65
meenakshibathuluri <meenakshibathuluri@gmail.com>
2025-11-12 18:57:58 +0530

    javascript program uploaded

---
+++ Day-12/script.js
@@ -0,0 +1,27 @@
+//let elements = document.getElementsByTagName("button");
+//console.log(elements);
+
+//console.log(elements[0].innerHtml);
+
+
+//elements[0].innerHtml = "<h1>Hello</h1>";
+//elements[1].innerHtml = "<h1>Hello</h1>";
+
+
+//let element = document.querySelector("button");
+//console.log(element);
+
+//let element1 = document.querySelectorAll("button");
+//console.log(element1);
+
+//let fristDiv = document.getElementById("div1");
+//fristDiv.innerText = "Div1";
+
+//let secondDiv = document.getElementById("div2");
+//secondDiv.innerText = "Div2";
+
+//let thirdDiv = document.getElementById("div3");
+//thirdDiv.innerText = "Div3";
+
+
+
