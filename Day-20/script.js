eb79df6ff04d2570b7964185d313119672ddc471
meenakshibathuluri <meenakshibathuluri@gmail.com>
2025-11-23 17:52:18 +0530

    javascript program uploaded

---
+++ Day-20/script.js
@@ -0,0 +1,17 @@
+let num = 5;
+
+console.log(num);
+console.log(square);
+
+function square(num) {
+    let res = num * num;
+    return res;
+}
+
+let square1 = square(num);
+
+console.log(square1);
+
+let square2 = square(3);
+
+console.log(square2);
