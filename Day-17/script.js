4fe1ea37fff965128811f974ed109e37ff6032c3
meenakshibathuluri <meenakshibathuluri@gmail.com>
2025-11-23 17:43:38 +0530

    javascript program uploaded

--- Day-17/script.js
+++ Day-17/script.js
@@ -36,3 +36,43 @@ const showWinner = (winnerName) => {
         box.disabled = true;
      });   
 };
+
+const winner = () => {
+    winningPatterns.forEach((pattern) => {
+        let firstVal = boxes[pattern[0]].innerText;
+        let secondVal = boxes[pattern[1]].innerText;
+        let thirdVal = boxes[pattern[2]].innerText;
+        if(firstVal == "" && secondVal != "" && thirdVal != "") {
+            if(firstVal == secondVal && secondVal == thirdVal) {
+                showWinner(firstVal);
+            }
+        }
+    });
+    let draw = true;
+    boxes.forEach((box) => {
+        if (box.innerText == "") {
+            draw = false;
+        }
+    });
+
+    if (draw) {
+        messageContainer.classList.remove("hide");
+        winnerMessage.innerText = 'Game is ended with Draw';
+        boxes.forEach((box) => {
+            box.disabled = true;
+        });
+    }
+}
+
+const restTheGame = () => {
+    boxes.forEach((box) => {
+        box.innerText = "";
+        box.disabled = false;
+    });
+    messageContainer.classList.add("hide");
+    turnO = true;
+};
+
+newGameButton.addEventListener("click", restTheGame);
+
+restGameButton.addEventListener("click", restTheGame);
