fa0eff307f7390efc15b92df49ae06ab6bf3e777
meenakshibathuluri <meenakshibathuluri@gmail.com>
2025-11-25 17:59:05 +0530

    javascript program uploaded

--- Day-21/script.js
+++ Day-21/script.js
@@ -33,6 +33,25 @@ function Hello() {
 
 //setTimeout(Hello, 3000);// function timeout(delay time)
 
+//setTimeout(() => {
+  //  console.log("meenakshi");
+    //console.log("D/o gopal");
+//}, 3000);
+
+//function sum(a,b) {
+  //  console.log(a + b);
+//}
+
+//function sub(a,b) {
+  //  console.log(a - b);
+//}
+
+//function calculator(a,b, calcfunction) {
+  //  calcfunction(a,b);
+//}
+//calculator(10,20,sum);
+//calculator(10,20,sub);
+
 setTimeout(() => {
     console.log("meenakshi");
     console.log("D/o gopal");
